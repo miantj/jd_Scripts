420433c555456519d60295d9d22a69e4a6914659
Dylan.Liu <Dylan.Liu>
2021-08-03 21:53:59 +0800

    店铺签到token更新

--- jd_speed_redpocke.js
+++ jd_speed_redpocke.js
@@ -24,8 +24,8 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let cookiesArr = [], cookie = '', message;
-const linkId = "";
-const signLinkId = '';
+const linkId = "9wdf1YTT2L59Vr-meKskLA";
+const signLinkId = '9WA12jYGulArzWS7vcrwhw';
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
     cookiesArr.push(jdCookieNode[item])
