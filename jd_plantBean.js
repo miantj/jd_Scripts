5f95b82a142c0bd799e1387cbd50f69cd2ff1e3d
Dylan.Liu <Dylan.Liu>
2021-08-18 14:37:22 +0800

    update

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -29,7 +29,7 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
 //此此内容是IOS用户下载脚本到本地使用，填写互助码的地方，同一京东账号的好友互助码请使用@符号隔开。
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
 let shareCodes = [ // IOS本地脚本用户这个列表填入你要助力的好友的shareCode
-    ''
+    '3a4uux47avyq3s5w555rpd7sdvxsjwn4kcn6fsi@nkvdrkoit5o64ovkh3niktqgsmajms6arjuadxi@rck7e7626u6ajw5o6nglvbyvf2q5mvbk5vj422a'
 ]
 let allMessage = ``;
 let currentRoundId = null;//本期活动id
