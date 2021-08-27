e8c7b0d970ec5bbe4f11eebac45db001cd960256
Dylan <58234511@qq.com>
2021-08-27 13:34:51 +0800

    fix plan

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -604,7 +604,7 @@ function requireConfig() {
     } else {
       if ($.getdata('jd_plantbean_inviter')) $.shareCodesArr = $.getdata('jd_plantbean_inviter').split('\n').filter(item => !!item);
       console.log(`\nBoxJs设置的${$.name}好友邀请码:${$.getdata('jd_plantbean_inviter') ? $.getdata('jd_plantbean_inviter') : '暂无'}\n`);
-    //}
+    }
     // console.log(`\n种豆得豆助力码::${JSON.stringify($.shareCodesArr)}`);
     console.log(`您提供了${$.shareCodesArr.length}个账号的种豆得豆助力码\n`);
     resolve()
