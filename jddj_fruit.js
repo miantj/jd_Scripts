b470ed4ad8185fe4e83909e9fd325318995ec140
Dylan <58234511@qq.com>
2021-08-30 11:40:27 +0800

    到家果园关闭通知

--- jddj_fruit.js
+++ jddj_fruit.js
@@ -10,7 +10,7 @@ boxjs订阅地址:https://gitee.com/passerby-b/javascript/raw/master/JD/passerby
 cron "10 0,3,8,11,17 * * *" script-path=https://raw.githubusercontent.com/passerby-b/JDDJ/main/jddj_fruit.js,tag=京东到家果园任务
 */
 
-let isNotify = true;//是否通知,仅限nodejs
+let isNotify = false;//是否通知,仅限nodejs
 let ckPath = './jdCookie.js';//ck路径,环境变量:JDDJ_CKPATH
 
 const $ = new API("jddj_fruit");
