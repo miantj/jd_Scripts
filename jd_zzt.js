a1f1183f40d671b8f1e091fad9406fa4863256b7
Dylan <58234511@qq.com>
2021-11-07 20:44:21 +0800

    制造团投骰子

--- jd_zzt.js
+++ jd_zzt.js
@@ -97,8 +97,8 @@ async function main() {
     authorizationInfo[$.UserName] = authres.access_token;
     tokenInfo[$.UserName] = $.token;
     console.log(`昵称：${user.nickname},剩余金币值：${user.coins}，助力码：${user.id}\n`);
-    if(user.is_all_map === 1){
-        console.log(`已到达终点`);
+    if(user.is_all_map === 2){
+        console.log(`已全部通关`);
         return;
     }
     let taskList = await taskUrl("shop_products", "");
