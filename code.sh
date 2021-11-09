2eaab09f640aa7ba0ca1e5451015df5dd9bf339b
Dylan <58234511@qq.com>
2021-11-09 16:01:50 +0800

    update

--- code.sh
+++ code.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ## 导入通用变量与函数
-#获取助力码
 #15 12,23 * * * code.sh 
+#new Env('获取助力码');
 grep 6dylan6 /ql/config/task_before.sh
 if [[ $? != 0 ]];then
 \cp /ql/repo/6dylan6_jdpro/docker/task_before.sh /ql/config/
