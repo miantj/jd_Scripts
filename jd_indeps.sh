cd1caad1d0c3c6b0864129219bea2286ceeb2394
Dylan <58234511@qq.com>
2021-11-26 10:45:40 +0800

    add财富岛珍珠兑换；依赖自动安装

---
+++ jd_indeps.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#依赖安装，运行一次就好
+#0 8 * 1 * jd_indeps.sh
+#const $ = new Env('依赖安装');
+#
+echo "安装脚本所需依赖，首次拉库运行一次即可"
+echo "开始安装............"
+
+pnpm install -g png-js
+pnpm install -g date-fns
+pnpm install -g axios
+pnpm install -g crypto-js
+pnpm install -g ts-md5
+pnpm install -g tslib
+pnpm install -g @types/node
+pnpm install -g requests
+pnpm install -g jsdom
+pnpm install -g moment
+
+echo "安装完成!“
