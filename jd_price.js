3134aee2f9deb4a58995f4d4ad30651e9768cd7e
Dylan <58234511@qq.com>
2021-12-01 10:43:30 +0800

    京东保价reup

--- jd_price.js
+++ jd_price.js
@@ -8,11 +8,11 @@
 41 12 * * * https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_price.js, tag=京东保价, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
 ================Loon==============
 [Script]
-cron "41 12* * *" script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_price.js,tag=京东保价
+cron "41 12 * * *" script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_price.js,tag=京东保价
 ===============Surge=================
-京东保价 = type=cron,cronexp="41 12* * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_price.js
+京东保价 = type=cron,cronexp="41 12 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_price.js
 ============小火箭=========
-京东保价 = type=cron,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_price.js, cronexpr="41 0,12,23 * * *", timeout=3600, enable=true
+京东保价 = type=cron,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_price.js, cronexpr="41 12 * * *", timeout=3600, enable=true
  */
 const $ = new Env('京东保价');
 const notify = $.isNode() ? require('./sendNotify') : '';
@@ -56,7 +56,9 @@ const JD_API_HOST = 'https://api.m.jd.com/';
         continue
       }
       await price()
-      await $.wait(2000)
+      if (i != cookiesArr.length - 1) {
+        await $.wait(20000)
+      }
     }
   }
   if (allMessage) {
@@ -167,14 +169,16 @@ async function jstoken() {
     pretendToBeVisual: true,
     virtualConsole
   };
-  const { window } = new JSDOM(``, options);
-  const jdPriceJs = await downloadUrl("https://js-nocaptcha.jd.com/statics/js/main.min.js")
+  // const { window } = new JSDOM(``, options);
+  // const jdPriceJs = await downloadUrl("https://js-nocaptcha.jd.com/statics/js/main.min.js")
+  const dom = new JSDOM(`<body><script src="https://js-nocaptcha.jd.com/statics/js/main.min.js"></script></body>`, options);
+  await $.wait(1000)
   try {
-    window.eval(jdPriceJs)
-    window.HTMLCanvasElement.prototype.getContext = () => {
-      return {};
-    };
-    $.jab = new window.JAB({
+    // window.eval(jdPriceJs)
+    // window.HTMLCanvasElement.prototype.getContext = () => {
+    //   return {};
+    // };
+    $.jab = new dom.window.JAB({
       bizId: 'jdjiabao',
       initCaptcha: false
     })
