273cdede7ab6d861a89b24d28f3582b8af3783a6
Dylan <58234511@qq.com>
2021-12-14 13:41:43 +0800

    京喜系列活动更新

--- jx_sign.js
+++ jx_sign.js
@@ -492,7 +492,7 @@ async function requestAlgo() {
       "expandParams": ""
     })
   }
-  new Promise(async resolve => {
+  return new Promise(async resolve => {
     $.post(options, (err, resp, data) => {
       try {
         if (err) {
