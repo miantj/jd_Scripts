42f03559bf63255f24ab4094cea5254151275dd4
Dylan <58234511@qq.com>
2021-12-15 08:38:08 +0800

    修复定时

--- jd_tyt_ck1.js
+++ jd_tyt_ck1.js
@@ -1,6 +1,6 @@
 /*
 #快速推一推
-0 0 * * * jd_tyt.js, tag=推一推, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
+2 0 * * * jd_tyt_ck1.js, tag=推一推, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
 注意：助力码每天会变，旧的不可用。
 助力逻辑：自动助力CK1
 入口-极速版-推推赚大钱  5元无门槛卷 大概需要50人助力
