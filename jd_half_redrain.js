886f3b9577f4922c5c6622af9b5c97753f6cbb9f
Dylan <58234511@qq.com>
2021-12-15 14:08:17 +0800

    新增京豆雨系列

--- jd_half_redrain.js
+++ jd_half_redrain.js
@@ -1,18 +1,6 @@
 /*
 半点京豆雨
-更新时间：2021-12-8
-脚本兼容: Quantumult X, Surge, Loon, JSBox, Node.js
-==============Quantumult X==============
-[task_local]
-#半点京豆雨
-30 20-23/1 * * * https://raw.githubusercontent.com/msechen/jdrain/main/jd_live_redrain.js, tag=半点京豆雨, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
-==============Loon==============
-[Script]
-cron "30 20-23/1 * * *" script-path=https://raw.githubusercontent.com/msechen/jdrain/main/jd_redrain_half.js,tag=半点京豆雨
-================Surge===============
-半点京豆雨 = type=cron,cronexp="30 20-23/1 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/msechen/jdrain/main/jd_redrain_half.js
-===============小火箭==========
-半点京豆雨 = type=cron,script-path=https://raw.githubusercontent.com/msechen/jdrain/main/jd_redrain_half.js, cronexpr="30 20-23/1 * * *", timeout=3600, enable=true
+31 20-23/1 * * * jd_half_redrain.js
 */
 const $ = new Env('半点京豆雨');
 let allMessage = '', id = '';
@@ -45,6 +33,10 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
     return;
     }
     for (let id of redIds) {
+    if (!/^RRA/.test(id)) {
+      console.log(`\nRRA: "${id}"不符合规则\n`);
+      continue;
+    }		
     console.log(`\n龙王就位:${id}，正在领取${hour}点30分京豆雨\n`);
     for (let i = 0; i < cookiesArr.length; i++) {
       if (cookiesArr[i]) {
