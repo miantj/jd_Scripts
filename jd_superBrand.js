5ed6df2efa61f6780b05012495750a22f5dac9ab
Dylan <58234511@qq.com>
2021-12-27 08:36:41 +0800

    特务

--- jd_superBrand.js
+++ jd_superBrand.js
@@ -255,7 +255,10 @@ function dealReturn(type, data) {
             }else if (data.code === '0' && data.data.bizCode === '108'){
                 $.canHelp = false;
                 console.log(`助力次数已用完`);
-            }else if (data.code === '0' && data.data.bizCode === '103'){
+            } else if (data.code === '0' && data.data.bizCode === '109') {
+                $.canHelp = false;
+                console.log(`不能自己给自己助力`);
+            } else if (data.code === '0' && data.data.bizCode === '103'){
                 console.log(`助力已满`);
                 $.codeInfo.time = 3;
             }else if (data.code === '0' && data.data.bizCode === '2001'){
