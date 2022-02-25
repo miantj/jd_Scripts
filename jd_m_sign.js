926afbbee0daf9f272d34d259f0db9af10c2b523
Dylan <58234511@qq.com>
2022-02-25 09:27:30 +0800

    update

--- jd_m_sign.js
+++ jd_m_sign.js
@@ -67,6 +67,8 @@ async function jdsign() {
     await $.wait(2000)
     await getInfo("https://pro.m.jd.com/mall/active/kPM3Xedz1PBiGQjY4ZYGmeVvrts/index.html");//陪伴
     await $.wait(2000)
+    await getInfo("https://prodev.m.jd.com/mall/active/412SRRXnKE1Q4Y6uJRWVT6XhyseG/index.html");//京东服装
+    await $.wait(2000)
     await getInfo("https://pro.m.jd.com/mall/active/3SC6rw5iBg66qrXPGmZMqFDwcyXi/index.html");//京东图书
 //     await getInfo("https://pro.m.jd.com/mall/active/ZrH7gGAcEkY2gH8wXqyAPoQgk6t/index.html");//箱包签到
 //     await $.wait(1000)
