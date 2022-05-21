79dfde5bc2b9c9b405ba72691721d7a02d7f2bfe
Dylan <58234511@qq.com>
2022-05-21 11:55:17 +0800

    update

--- jd_sign_graphics1.js
+++ jd_sign_graphics1.js
@@ -30,8 +30,8 @@ let JD_API_HOST = 'https://sendbeans.jd.com'
 const turnTableId = [
  //  { "name": "美妆-1", "id": 293, "shopid": 30284, "url": "https://sendbeans.jd.com/jump/index/" },
    { "name": "小米", "id": 1082, "shopid": 1000004123, "url": "https://sendbeans.jd.com/jump/index/" },
-   { "name": "美妆-3", "id": 1082, "shopid": 1000004123, "url": "https://sendbeans.jd.com/jump/index/" },
-   { "name": "翻牌", "id": 1082, "shopid": 1000004123, "url": "https://sendbeans.jd.com/jump/index/" },
+   //{ "name": "美妆-3", "id": 1082, "shopid": 1000004123, "url": "https://sendbeans.jd.com/jump/index/" },
+   { "name": "翻牌", "id": 1419, "shopid": 1000007205, "url": "https://sendbeans.jd.com/jump/index/" },
 ]
 
 !(async () => {
