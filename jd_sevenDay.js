92107eb102bb53c6f246e2670b7ea26c98a2b4ac
dylan <58234511@qq.com>
2022-09-01 08:54:10 +0800

    1

--- jd_sevenDay.js
+++ jd_sevenDay.js
@@ -16,7 +16,7 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 let cookiesArr = [], cookie = '', message = '';
 // https://lzkj-isv.isvjcloud.com/sign/sevenDay/signActivity?activityId=
 let activityIdList = [
-    '4dba6f1f51f34d5a85a4bf03353d4517',
+    //'4dba6f1f51f34d5a85a4bf03353d4517',
     //'f3207d49ac8e4fcfbd542befc1338916',
     //'ba2d672b1fc44814aef1c3ee4c3ac5fa',
     //'f46601936b40496b97a85de2b5ab97eb',
