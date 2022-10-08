997250ab733b7b2f7cfe04e4d0cc92401b33525c
Lyle <934854233@qq.com>
2022-10-08 10:12:26 +0800

    删除

--- jd_speed_signfree.js
+++ jd_speed_signfree.js
@@ -1,7 +1,7 @@
 /*
   入口>京东极速版>首页>签到免单
   京东极速版,先下单,第二天开始签到
-  18 8,20 * * * jd_speed_signfree.js 签到免单
+  16 8,20 * * * jd_speed_signfree.js 签到免单
 */
 const $ = new Env('京东极速版签到免单');
 const notify = $.isNode() ? require('./sendNotify') : '';
