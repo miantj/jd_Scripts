042f1ea0832c40903a9113d61552c6612a40956b
Lyle <934854233@qq.com>
2022-10-14 17:00:18 +0800

    .

--- jd_disable.py
+++ jd_disable.py
@@ -28,7 +28,7 @@ if not ipport:
     ipport = "localhost:5700"
 else:
     ipport = ipport.lstrip("http://").rstrip("/")
-sub_str = os.getenv("RES_SUB", "KingRan_KR")
+sub_str = os.getenv("RES_SUB", "miantj_jd_Scripts")
 sub_list = sub_str.split("&")
 res_only = os.getenv("RES_ONLY", True)
 headers = {
