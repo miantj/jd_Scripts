722d2caf31a82aefddd8d6f4ed580eb701868411
2024 <glukvbill@gmail.com>
2024-05-15 23:04:51 +0800

    1

--- jd_fruit_new.js
+++ jd_fruit_new.js
@@ -3,7 +3,7 @@
 ==========================Quantumultx=========================
 [task_local]
 #jd新农场
-10 6,16 * * * jd_fruit_new.js
+10 6,12,17 * * * jd_fruit_new.js
 
 */
 const $ = new Env('新农场任务');
